809a7ec561313210d8cb6380fd159ab2b8881a02
Tom Janssens <Tom@corebvba.be>
2013-08-27 18:07:02 +0200

    Extracted the reference repo data to the test_helper
    So we can reuse this for other tests...

--- test/git_test.exs
+++ test/git_test.exs
@@ -4,11 +4,6 @@ defmodule GitTest do
 	use ExUnit.Case
 	import PathHelpers
 
-	def expected_commits do 
-        [{"3ce6eec", "Initial commit"}, 
-         {"90e0504", "Update the README with the correct reference to the gitut project"}, 
-         {"8092953", "[skip] Let's try to skip an entry"}, 
-         {"afcc832", "Do some more fine-tuning to the readme"}]	end
 	
 	test "current commit id from a non-git folder should return nil" do
 		with_tmp_folder do 
@@ -24,21 +19,42 @@ defmodule GitTest do
 
 	test "clone a github project and verify commits exist" do
 		with_tmp_folder do
-			Git.clone "https://github.com/ToJans/testfixture-repo.git"
-			assert Git.commits == expected_commits
+			Git.clone ReferenceRepo.url
+			assert Git.commits == ReferenceRepo.expected_commits
 		end
 	end
 
 	test "clone a github project, checkout a commit and verify a message" do
 		with_tmp_folder do
 			Git.clone "https://github.com/ToJans/testfixture-repo.git"
-			Git.checkout "90e0504"
-			assert Git.current_commit == "90e0504"
-			readme = File.read!("README.md")
-			assert String.contains?(readme,"(http://github.com/ToJans/gitut)")
-			assert Git.commit_message("afcc832")== 
-				"Do some more fine-tuning to the readme\n\nIt does not mather what exactly in this case"
+			Git.checkout ReferenceRepo.second_commit_id
+			assert Git.current_commit == ReferenceRepo.second_commit_id
+			assert ReferenceRepo.second_version_of_readme?()
+			assert Git.commit_message(ReferenceRepo.forth_commit_id)== 
+				ReferenceRepo.forth_commit_message
+				
 		end
 	end
+end
+
+defmodule ReferenceRepo do
+	def url, do: "https://github.com/ToJans/testfixture-repo.git"
+	def expected_commits do 
+        [{"3ce6eec", "Initial commit"}, 
+         {"90e0504", "Update the README with the correct reference to the gitut project"}, 
+         {"8092953", "[skip] Let's try to skip an entry"}, 
+         {"afcc832", "Do some more fine-tuning to the readme"}]	
+    end
+    def second_commit_id, do: "90e0504"
+    def second_version_of_readme? do
+    	"README.md"
+    		|> File.read!()
+    	  	|> String.contains?("(http://github.com/ToJans/gitut)")
+    end
+    def forth_commit_id, do: "afcc832"
+
+    def forth_commit_message do
+    	"Do some more fine-tuning to the readme\n\nIt does not mather what exactly in this case"
+    end
 
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -24,7 +24,34 @@ defmodule PathHelpers do
 		  |> List.flatten() 
 		  |> String.from_char_list!()
 	end
+end
 
+defmodule ReferenceRepo do
+	def url, do: "https://github.com/ToJans/testfixture-repo.git"
+	def expected_commits do 
+        [{"3ce6eec", "Initial commit"}, 
+         {"90e0504", "Update the README with the correct reference to the gitut project"}, 
+         {"8092953", "[skip] Let's try to skip an entry"}, 
+         {"afcc832", "Do some more fine-tuning to the readme"}]	
+    end
+    def first_commit_id, 	do: expected_commits |> List.at(0) |> elem(0)
+    def second_commit_id, 	do: expected_commits |> List.at(1) |> elem(0)
+    def third_commit_id, 	do: expected_commits |> List.at(2) |> elem(0)
+    def forth_commit_id, 	do: expected_commits |> List.at(3) |> elem(0)
+
+    def second_version_of_readme? do
+    	"README.md"
+    		|> File.read!()
+    	  	|> String.contains?("(http://github.com/ToJans/gitut)")
+    end
+
+    def forth_commit_message do
+    	"Do some more fine-tuning to the readme\n\nIt does not mather what exactly in this case"
+    end
+
+    def gitut_commits do
+    	expected_commits |> List.delete(2)
+    end
 
 end
 
